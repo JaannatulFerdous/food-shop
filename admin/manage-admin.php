c3a28af24a1e22d66a854edf31b5cc6a5f8bed1b
JaannatulFerdous <jannaatulferdous14@gmail.com>
2023-05-05 12:39:32 +0600

    added admin pannel

---
+++ admin/manage-admin.php
@@ -0,0 +1,13 @@
+<?php include('partials/menu.php'); ?>
+
+        <!-- Main content section starts-->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Manage Admin</h1>
+
+            </div>
+
+        </div>
+        <!-- Menu content ends-->
+
+<?php include('partials/footer.php'); ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+ <!-- Footer section starts-->
+ <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2023 All rights reserved to, Food Shop. Developed By - <a href="">Jannatul Ferdous</a></p>
+            </div>
+
+        </div>
+        <!-- Footer section ends-->
+    </body>
+</html>
